bfba749d02c750b0159a602d97a6c858e5b1d15f
MiguelCordero7 <lavanderiamiguel13@gmail.com>
2023-02-07 05:10:07 -0700

    Concatenar

---
+++ FundamentosC/Concatenar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace concatena
+{
+    class Concatenar
+    {
+         static void Main(String[] args)
+        {
+            int edad = 21;
+            String nombre = "miguel";
+
+            Console.WriteLine($"Hola, mi nombre es {nombre} y tengo una edad de {edad}");
+        }
+    }
+}
---
+++ FundamentosC/HolaMundo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Unidad_1;
+
+class HolaMundo
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Bienvenidos a C#");
+    }
+}
